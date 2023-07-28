bafe8264394c0ca92cbeafc6ad09b0945a2c3b9d
Aryainguz <aryaninguz369@gmail.com>
2023-07-28 14:52:26 +0530

    "sendgrid added

--- app.js
+++ app.js
@@ -1,47 +1,19 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const axios = require("axios")
+const sgMail = require("@sendgrid/mail")
 
 require("dotenv").config()
+
+const API_KEY = process.env.KEY
+sgMail.setApiKey(API_KEY)
+
 
 const PORT = 3000
 const app = express()
 const reciever_email = []
 
 const verdict = []
-const PERMA_questions = [
-  {},
-{"question":"How often do you find yourself happy?","option1":"Rarely","option2":"Frequently","code":"P"},
-{"question":"Do you often experience feelings of excitement and enthusiasm?","option1":"Rarely","option2":"Frequently","code":"P"},
-{"question":"How frequently do you feel grateful for the good things in your life?","option1":"Rarely","option2":"Frequently","code":"P"},
-
-{"question":"Do you actively seek out new challenges to keep yourself engaged?","option1":"Rarely","option2":"Frequently","code":"E"},
-{"question":"How often do when you're doing activities you enjoy, you lose track of time?","option1":"Rarely","option2":"Frequently","code":"E"},
-{"question":"How often do you get completely absorbed in what you're doing?","option1":"Rarely","option2":"Frequently","code":"E"},
-
-{"question":"Do you feel like you have a support system of friends and family?","option1":"Negative","option2":"Affiramative","code":"R"},
-{"question":"How often do you spend quality time with people who uplift and inspire you?","option1":"Negative","option2":"Affiramative","code":"R"},
-{"question":"Do you feel comfortable sharing your thoughts and feelings with others?","option1":"Negative","option2":"Affiramative","code":"R"},
-
-
-{"question":" Do you feel a sense of purpose and direction in your life?","option1":"Negative","option2":"Affirmative","code":"M"},
-{"question":" Do you feel that your life has a deeper meaning beyond just day-to-day activities?","option1":"Negative","option2":"Affirmative","code":"M"},
-{"question":"Do you like going to college/school/office ?","option1":"Negative","option2":"Affirmative","code":"M"},
-
-
-{"question":"Have you achieved any major goals ?","option1":"Negative","option2":"Affirmative","code":"A"},
-{"question":"Do you think your Parents are proud of you?","option1":"Negative","option2":"Affirmative","code":"A"},
-{"question":"Do you think people respect you ?","option1":"Negative","option2":"Affirmative","code":"A"},
-
-
-{"question":"Do you feel jealous at some times ?","option1":"Negative","option2":"Affiramative","code":"P"},
-{"question":"Do you feel you need a partner?","option1":"Negative","option2":"Affiramative","code":"R"},
-{"question":"Do you spend negative time on socials?","option1":"Negative","option2":"Affiramative","code":"M"},
-{"question":"Can you dance without fear in public?","option1":"Negative","option2":"Affiramative","code":"E"},
-{"question":"Do you think you have done good in your life so far ?","option1":"Negative","option2":"Affiramative","code":"A"},
-]
-
-
 
 app.use(express.static(__dirname+"/Public"));  //use this to use css files insie templates 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -49,23 +21,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('views', __dirname + '/views');
 app.set("view engine","ejs");
-
-//Email
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  service:"gmail",
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: true,
-  auth: {
-    user: process.env.FROM,
-    pass: process.env.PASS
-  }
-});
-
-
-
 
 
 app.get("/",(req,res)=>{
@@ -103,19 +58,17 @@ app.post("/result",(req,res)=>{
   }
 
 
-  const mailOptions = {
-        from:process.env.FROM,
-        to:reciever_email[0],
-        subject:"Euphoria Check",
-        text:`Here are your PERMA scores\n\nPositive Emotions: ${p} \nEngagement: ${e} \nRelationships: ${r} \nMeaning: ${m} \nAccomplishment: ${a} \n\n ${verdict[0]} \n\n Aryan Inguz.`
+    const message = {
+      to : reciever_email[0],
+      from : process.env.FROM,
+      subject:"Euphoria Check",
+      text:`Here are your PERMA scores\n\nPositive Emotions: ${p} \nEngagement: ${e} \nRelationships: ${r} \nMeaning: ${m} \nAccomplishment: ${a} \n\n ${verdict[0]} \n\n Aryan Inguz.`
     }
-    transporter.sendMail(mailOptions,(error,info)=>{
-        if(error){
-            res.send(error)
-        }else{
-            res.send("mail sent!"+info.response)
-        }
-    })
+     sgMail
+     .send(message)
+     .then((response)=>console.log("Email sent!"))
+     .catch((error)=>console.log(error.message))
+
   res.render("result",{pos:p,eng:e,mea:m,rel:r,acc:a})
 })
 
